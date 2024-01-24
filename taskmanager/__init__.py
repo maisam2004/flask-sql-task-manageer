62c67ce9e7f86e18a2c09c7ffb7b262d6786eb1f
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-01-24 13:40:58 +0000

    Create routs.py to run base html

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,6 +1,15 @@
 import os 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy #noqa
 if os.path.exists("env.py"):
     import env
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SQLAlCHEMY_DATABAE_URL"] = os.environ.get("DB_URL")
+
+db = SQLAlchemy(app)
+
+from taskmanager import routes #should come after all up importdd
+
 
