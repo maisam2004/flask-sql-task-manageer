d9e4e05f9ee5f8bd0950a62ae664878a8878af45
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-01-24 14:21:28 +0000

    Update inite texts

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,15 +1,14 @@
-import os 
+import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy #noqa
+from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
-    import env
+    import env  # noqa
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLAlCHEMY_DATABAE_URL"] = os.environ.get("DB_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
 
-from taskmanager import routes #should come after all up importdd
-
-
+from taskmanager import routes  # noqa
