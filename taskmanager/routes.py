8b1503dcc5e988c0075a110b4176f1c6e53b869f
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-01-25 14:44:38 +0000

    Update routes to read categories and show on categories

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -7,8 +7,10 @@ def home():
     return render_template("tasks.html")
 
 @app.route("/categories")
+
 def categories():
-    return render_template("categories.html")
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html",categories = categories)
 
 @app.route("/add_category",methods = ["GET","POST"])
 def add_category():
