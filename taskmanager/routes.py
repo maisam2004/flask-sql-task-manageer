2cf314bfa256f314d9a966c07b765581f08668ad
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-01-25 15:28:53 +0000

    Update data by using argument in address bar

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -9,7 +9,10 @@ def home():
 @app.route("/categories")
 
 def categories():
+    #categories = list(Category.query.order_by(Category.id).all())
+    #Category.query.all() grab all data for this db part
     categories = list(Category.query.order_by(Category.category_name).all())
+    
     return render_template("categories.html",categories = categories)
 
 @app.route("/add_category",methods = ["GET","POST"])
@@ -20,3 +23,8 @@ def add_category():
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("add_category.html")
+
+@app.route("/edit_category/<int:category_id>",methods = ["GET","POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    return render_template("edit_category.html",category = category)
